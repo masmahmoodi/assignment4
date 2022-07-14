e4ce9085d975f0df32ccab547dd3a1e31b22dfa6
masmahmoodi <108240269+masmahmoodi@users.noreply.github.com>
2022-07-14 16:25:41 +0430

    Add files via upload

---
+++ multi.php
@@ -0,0 +1,100 @@
+<?php
+$table = 2;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+while($i <= $length)
+{
+ echo "$i * $table = ".$i * $table. "<br>";
+ $i++;
+}
+
+$table = 3;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+while($i <= $length)
+{
+ echo "$i * $table = ".$i * $table. "<br>";
+ $i++;
+}
+
+$table = 4;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+while($i <= $length)
+{
+ echo "$i * $table = ".$i * $table. "<br>";
+ $i++;
+}
+
+$table = 5;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+while($i <= $length)
+{
+ echo "$i * $table = ".$i * $table. "<br>";
+ $i++;
+}
+
+$table = 6;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+while($i <= $length)
+{
+ echo "$i * $table = ".$i * $table. "<br>";
+ $i++;
+}
+
+$table = 7;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+while($i <= $length)
+{
+ echo "$i * $table = ".$i * $table. "<br>";
+ $i++;
+}
+
+$table = 8;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+while($i <= $length)
+{
+ echo "$i * $table = ".$i * $table. "<br>";
+ $i++;
+}
+
+$table = 9;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+while($i <= $length)
+{
+ echo "$i * $table = ".$i * $table. "<br>";
+ $i++;
+}
+
+$table = 10;
+$length = 10;
+$i = 1;
+
+echo "Multiplication table: $table <br>";
+while($i <= $length)
+{
+ echo "$i * $table = ".$i * $table. "<br>";
+ $i++;
+}
+?>
---
+++ rec.php
@@ -0,0 +1,26 @@
+﻿<?php
+// PHP code for hollow rectangle
+
+// Function to print hollow rectangle
+function print_rectangle($n, $m)
+{
+    $i;
+    $j;
+    for ($i = 1; $i <= $n; $i++) {
+        for ($j = 1; $j <= $m; $j++) {
+            if (
+                $i == 1 || $i == $n ||
+                $j == 1 || $j == $m
+            )
+                echo ("*");
+            else
+                echo (" ");
+        }
+        echo ("\n");
+    }
+}
+
+// Driver Code
+$rows = 6;
+$columns = 20;
+print_rectangle($rows, $columns);
